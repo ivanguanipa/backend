f20a5be19e5758466935a2003c61e7082cdd5e37
Ivan Guanipa <ivanguanipa@gmail.com>
2022-04-09 09:16:54 -0400

    smell

--- src/infraestructura/migraciones/1583873676081-MigracionUsuario.ts
+++ src/infraestructura/migraciones/1583873676081-MigracionUsuario.ts
@@ -4,10 +4,11 @@ export class MigracionUsuario1583873676081 implements MigrationInterface {
   name = 'MigracionUsuario1583873676081';
 
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query(
-      'CREATE TABLE IF NOT EXISTS `USUARIO` (`id` int NOT NULL AUTO_INCREMENT, `nombre` varchar(255) NOT NULL, `clave` varchar(255) NOT NULL, `fechaCreacion` datetime NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB',
-      undefined,
-    );
+    const query =
+      'CREATE TABLE IF NOT EXISTS `USUARIO` (`id` int NOT NULL AUTO_INCREMENT, ' +
+      ' `nombre` varchar(255) NOT NULL, `clave` varchar(255) NOT NULL,' +
+      ' `fechaCreacion` datetime NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB';
+    await queryRunner.query(query, undefined);
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
